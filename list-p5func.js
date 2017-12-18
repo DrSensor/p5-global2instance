489b804bc2b58d0cb7afb8f55f4b31cd03ba1356
Fahmi Akbar Wildana <fahmi.akbar.w@mail.ugm.ac.id>
2017-12-18 22:20:58 +0700

    Add complete list of p5 funcs and vars based on References page

--- list-p5func.js
+++ list-p5func.js
@@ -2,6 +2,7 @@ const p5scopeFuncs = [
   'preload',
   'setup',
   'draw',
+  'windowResized',
   'mouseMoved',
   'mouseDragged',
   'mousePressed',
@@ -11,15 +12,23 @@ const p5scopeFuncs = [
   'mouseWheel',
   'touchStarted',
   'touchMoved',
-  'touchEnded'
+  'touchEnded',
+  'keyPressed',
+  'keyReleased',
+  'keyTyped',
+  'deviceMoved',
+  'deviceTurned',
+  'deviceShaken'
 ]
 
 const p5callFuncs = [
   'createCanvas',
-  'createVector',
   'createGraphics',
+  'resizeCanvas',
+  'noCanvas',
+  'blendMode',
 
-  // Color.settings
+  // Color.Settings
   'background',
   'clear',
   'colorMode',
@@ -27,6 +36,18 @@ const p5callFuncs = [
   'noFill',
   'noStroke',
   'stroke',
+
+  // Color.Creating_and_Reading
+  'alpha',
+  'blue',
+  'brightness',
+  'color',
+  'green',
+  'hue',
+  'lerpColor',
+  'lightness',
+  'red',
+  'saturation',
 
   // Structure
   'remove',
@@ -36,7 +57,7 @@ const p5callFuncs = [
   'pop',
   'redraw',
 
-  // Shape.2d_primitives
+  // Shape.2D_Primitives
   'arc',
   'ellipse',
   'line',
@@ -45,9 +66,207 @@ const p5callFuncs = [
   'rect',
   'triangle',
 
+  // Shape.3D_Models
+  'loadModel',
+  'model',
+
+  // Shape.3D_Primitives
+  'plane',
+  'box',
+  'sphere',
+  'cylinder',
+  'cone',
+  'ellipsoid',
+  'torus',
+
+  // Shape.Attributes
+  'ellipseMode',
+  'noSmooth',
+  'rectMode',
+  'smooth',
+  'strokeCap',
+  'strokeJoin',
+  'strokeWeight',
+
+  // Shape.Curves
+  'bezier',
+  'bezierDetail',
+  'bezierPoint',
+  'bezierTangent',
+  'curve',
+  'curveDetail',
+  'curveTightness',
+  'curvePoint',
+  'curveTangent',
+
+  // Shape.Vertex
+  'beginContour',
+  'beginShape',
+  'bezierVertex',
+  'curveVertex',
+  'endContour',
+  'endShape',
+  'quadraticVertex',
+  'vertex',
+
+  // Environment
+  'print',
+  'cursor',
+  'frameRate',
+  'noCursor',
+  'fullscreen',
+  'pixelDensity',
+  'displayDensity',
+  'getURL',
+  'getURLPath',
+  'getURLParams',
+
+  // Transform
+  'applyMatrix',
+  'resetMatrix',
   'rotate',
+  'rotateX',
+  'rotateY',
+  'rotateZ',
   'scale',
-  'translate'
+  'shearX',
+  'shearY',
+  'translate',
+
+  // Data.Array_Functions
+  'append',
+  'arrayCopy',
+  'concat',
+  'reverse',
+  'shorten',
+  'shuffle',
+  'sort',
+  'splice',
+  'subset',
+
+  // Data.Conversion
+  'float',
+  'int',
+  'str',
+  'boolean',
+  'byte',
+  'char',
+  'unchar',
+  'hex',
+  'unhex',
+
+  // Data.String_Functions
+  'join',
+  'match',
+  'matchAll',
+  'nf',
+  'nfc',
+  'nfp',
+  'nfs',
+  'split',
+  'splitTokens',
+  'trim',
+
+  // Events
+  'setMoveThreshold',
+  'setShakeThreshold',
+  'keyIsDown',
+
+  // Image
+  'createImage',
+  'saveCanvas',
+  'saveFrames',
+  'loadImage',
+  'image',
+  'tint',
+  'noTint',
+  'imageMode',
+  'blend',
+  'copy',
+  'filter',
+  'get',
+  'loadPixels',
+  'set',
+  'updatePixels',
+
+  // IO
+  'loadJSON',
+  'loadStrings',
+  'loadTable',
+  'loadXML',
+  'httpGet',
+  'httpPost',
+  'httpDo',
+  'createWriter',
+  'write',
+  'print',
+  'flush',
+  'close',
+  'save',
+  'saveJSON',
+  'saveStrings',
+  'saveTable',
+  'downloadFile',
+
+  // Math
+  'createVector',
+  'constrain',
+  'dist',
+  'lerp',
+  'mag',
+  'map',
+  'norm',
+  'sq',
+  'noise',
+  'noiseDetail',
+  'noiseSeed',
+  'degrees',
+  'radians',
+  'angleMode',
+
+  // Typography
+  'textAlign',
+  'textLeading',
+  'textSize',
+  'textStyle',
+  'textWidth',
+  'textAscent',
+  'textDescent',
+  'loadFont',
+  'text',
+  'textFont',
+
+  // Lights_Camera
+  'camera',
+  'perspective',
+  'ortho',
+  'ambientLight',
+  'directionalLight',
+  'pointLight',
+  'loadShader',
+  'shader',
+  'normalMaterial',
+  'texture',
+  'ambientMaterial',
+  'specularMaterial'
+]
+
+const p5Instance = [
+  'p5.Color',
+  'p5.Element',
+  'p5.Graphics',
+  'p5.TypedDict',
+  'p5.NumberDict',
+  'p5.Table',
+  'p5.TableRow',
+  'p5.XML',
+  'p5.Vector',
+  'p5.Font',
+  'p5.Shader',
+  'p5.Texture',
+  'p5.Geometry',
+  'p5.Matrix',
+  'p5.RendererGL'
 ]
 
 const p5vars = [
@@ -58,17 +277,81 @@ const p5vars = [
   'TAU',
   'TWO_PI',
 
+  // Environment
+  'frameCount',
+  'focused',
+  'displayWidth',
+  'displayHeight',
+  'windowWidth',
+  'windowHeight',
   'width',
   'height',
+
+  // Events.Mobile
+  'deviceOrientation',
+  'accelerationX',
+  'accelerationY',
+  'accelerationZ',
+  'pAccelerationX',
+  'pAccelerationY',
+  'pAccelerationZ',
+  'rotationX',
+  'rotationY',
+  'rotationZ',
+  'pRotationX',
+  'pRotationY',
+  'pRotationZ',
+  'turnAxis',
+  'touches',
+
+  // Events.Desktop
+  'keyIsPressed',
+  'key',
+  'keyCode',
   'mouseX',
-  'mouseY'
+  'mouseY',
+  'pmouseX',
+  'pmouseY',
+  'winMouseX',
+  'winMouseY',
+  'pwinMouseX',
+  'pwinMouseY',
+  'mouseButton',
+  'mouseIsPressed',
+
+  'pixels'
 ]
 
 // list of function that is better to use js stdfunc rather than p5
+// the purpose I left it here is in case need for warning in enforce mode
 const p5slowFuncs = [
+  'day',
+  'hour',
+  'minute',
   'millis',
-  'random'
+  'month',
+  'second',
+  'year',
+  'randomSeed',
+  'random',
+  'randomGaussian'
 ]
+
+// list of p5 function that overlap with browser std function
+const stdFuncs = {
+  'Math': [
+    'abs',
+    'ceil',
+    'exp',
+    'floor',
+    'log',
+    'max',
+    'min',
+    'pow',
+    'round',
+    'sqrt'
+  ]
+}
 
 const p5exprs = [
   ...p5callFuncs,
@@ -81,5 +364,7 @@ module.exports = {
   p5exprs,
   p5callFuncs,
   p5vars,
+  p5Instance,
+  stdFuncs,
   p5slowFuncs
 }
